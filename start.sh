732be580e8465562e6ee10315c235040365fa6c5
佐藤亮介 <srrrs@satouryousukes-MacBook-Air.local>
2022-05-26 06:40:22 +0900

    migrate

--- start.sh
+++ start.sh
@@ -4,9 +4,7 @@ set -e
 
 echo "run db migration"
 source /app/app.env
-echo "read app.env"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
-echo "finish migrate"
 
 echo "start the app"
 exec "$@"
