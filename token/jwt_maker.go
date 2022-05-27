4a30eeb21060289dd21b62595aa0578481ab144e
佐藤亮介 <srrrs@satouryousukes-MacBook-Air.local>
2022-05-27 15:47:42 +0900

    dbdocs

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -26,15 +26,17 @@ func NewJWTMaker(secretKey string) (Maker, error) {
 }
 
 // CreateToken creates a new token for a specific username and duration
-func (jwtMaker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (jwtMaker *JWTMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 
-	return jwtToken.SignedString([]byte(jwtMaker.secretKey))
+	token, err := jwtToken.SignedString([]byte(jwtMaker.secretKey))
+
+	return token, payload, err
 }
 
 // VerifyToken checks if the token is valid or not
